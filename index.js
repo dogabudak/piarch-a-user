ec7b1ecf4ff6f46f542218170538612dbc2901f2
doga <doga.budak@check24.de>
2021-01-18 10:05:00 +0100

    Refactored code

--- index.js
+++ index.js
@@ -1,90 +1,94 @@
-const Router = require('koa-router'),
-koa = require('koa'),
-koaBody = require('koa-body'),
-config = require('./resources/config.js'),
-mongojs = require('mongojs'),
-db = mongojs(config.mongo.url, ['users']),
-nano = require('nanomsg'),
-nanoReq = nano.socket('req');
+const Router = require('koa-router');
+const koa = require('koa');
+const koaBody = require('koa-body');
+const config = require('./resources/config.js');
+const mongojs = require('mongojs');
+const db = mongojs(config.mongo.url, ['users']);
+const nano = require('nanomsg');
+const nanoReq = nano.socket('req');
 
 //TODO token check must be a plug-in
 
-var app = new koa();
+const app = new koa();
 const route = new Router();
 
 app.listen(config.server.port);
 app.use(route.routes())
-   .use(route.allowedMethods());
-route.post('/update-user',function (ctx) {
-  return updateUserFunction(ctx).then(function(dataToReturn) {
-    ctx.body = dataToReturn;
-  });
+    .use(route.allowedMethods());
+
+route.post('/update-user', (ctx) => {
+    return updateUser(ctx).then((dataToReturn) => {
+        ctx.body = dataToReturn;
+    });
 });
 
 
-route.post('/update-location',koaBody(), function (ctx){
-  return updateCurrentLocation(ctx).then(function(dataToReturn) {
-    ctx.body = {};
-  });
+route.post('/update-location', koaBody(), (ctx) => {
+    return updateCurrentLocation(ctx).then(() => {
+        ctx.body = {};
+    });
 });
 
 nanoReq.connect(config.verificationUrl);
 
 
-function updateCurrentLocation(context) {
-  return new Promise(function (fulfill, reject) {
-  let body = context.request.body;
-  let userLocation = body.currentLocation;
+const updateCurrentLocation = (context) => {
+    //TODO  hande fullfill and rejected cases
+    return new Promise((fulfill, reject) => {
+        let body = context.request.body;
+        let userLocation = body.currentLocation;
 
-  userLocation.timestamp = new Date().toISOString();
-  let token ;
-  let userNameFromToken;
-  try{
-    token = body.token,
-    tokenArr = token.split('.'),
-    tokenClaims = new Buffer(tokenArr[1],'base64'),
-    userNameFromToken = (JSON.parse(tokenClaims)).sub;
-  } catch (err){
-    //TODO handle error
-  }
-  nanoReq.send('jwt '+ token);
-  nanoReq.on('data', function (buf) {
-    if (buf.toString() === 'true'){
-      db.users.findAndModify({
-    	query: { "username": userNameFromToken },
-    	update: { $push: { "locations": userLocation } },
-       new: true
-      }, function (err, doc) {
-  //TODO a return needed ?
-    })}
-  });
-
-
-  })
+        userLocation.timestamp = new Date().toISOString();
+        let token;
+        let userNameFromToken;
+        try {
+            token = body.token;
+            const tokenArr = token.split('.');
+            const tokenClaims = new Buffer(tokenArr[1], 'base64');
+            userNameFromToken = (JSON.parse(tokenClaims)).sub;
+        } catch (err) {
+            reject(err)
+            //TODO handle error
+        }
+        nanoReq.send('jwt ' + token);
+        nanoReq.on('data',  (buf) => {
+            if (buf.toString() === 'true') {
+                db.users.findAndModify({
+                    query: {"username": userNameFromToken},
+                    update: {$push: {"locations": userLocation}},
+                    new: true
+                }, (err, doc) => {
+                    //TODO a return needed ?
+                })
+            }
+        });
+    })
 }
 
 
-function updateUserFunction(req) {
-  return new Promise(function (fulfill, reject) {
-    try{
-      let token = req.token,
-      tokenArr = token.split('.'),
-      tokenClaims = new Buffer(tokenArr[1],'base64'),
-      userNameFromToken = (JSON.parse(tokenClaims)).sub;
+const updateUser = (req) => {
+    //TODO  hande fullfill and rejected cases
+    return new Promise((fulfill, reject) => {
+        try {
+            const token = req.token;
+            const tokenArr = token.split('.');
+            const tokenClaims = new Buffer(tokenArr[1], 'base64');
+            const userNameFromToken = (JSON.parse(tokenClaims)).sub;
 
-    } catch (err){
-      //TODO handle error
-    }
-    nanoReq.send('jwt '+token);
-    nanoReq.on('data', function (buf) {
-      if (buf.toString() === 'true'){
-        db.users.findAndModify({
-          query: { "username": userNameFromToken }
-	       // ,update: { $set: { tag: 'maintainer' } }
-}, function (err, doc, lastErrorObject) {
-          //TODO a return needed ?
-})
-      }
-    });
-  })
+        } catch (err) {
+            //TODO handle error
+        }
+        nanoReq.send('jwt ' + token);
+        nanoReq.on('data',  (buf) => {
+            if (buf.toString() === 'true') {
+                db.users.findAndModify({
+                    query: {"username": userNameFromToken}
+                    // ,update: { $set: { tag: 'maintainer' } }
+                }, (err, doc, lastErrorObject) => {
+                    console.log(err, doc, lastErrorObject)
+                    //TODO a return needed ?
+                })
+            }
+        });
+    })
 }
--- resources/config.js
+++ resources/config.js
@@ -10,11 +10,6 @@ module.exports = {
         'port': 3020,
         'path': '/update'
     },
-    'redis': {
-        'redis_ip': '127.0.0.1',
-        'redis_port':6379,
-        'db':4
-    },
     'authnUrl': '127.0.0.1:3018',
     'verificationUrl':'tcp://127.0.0.1:5608'
 }
